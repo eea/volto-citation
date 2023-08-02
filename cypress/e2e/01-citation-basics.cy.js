0123f35ce0fd39422e0bbdc38c36ee8e38486908
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-08-02 12:21:46 +0300

    increase code covreage

--- cypress/e2e/01-citation-basics.cy.js
+++ cypress/e2e/01-citation-basics.cy.js
@@ -47,10 +47,32 @@ describe('Blocks Tests', () => {
     cy.get('pre').contains('title = {Citation},');
     cy.get('pre').contains('howpublished = {http://www.eeacitation.com},');
     cy.get('.citation-block a').first().click();
+
     // Save
     cy.get('#toolbar-save').click();
-    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+    cy.wait(5000);
 
-    // then the page view should contain our changes
+    //test view
+    cy.get('blockquote').contains('(2023)');
+    cy.get('blockquote').contains('Citation');
+    cy.get('blockquote').contains('Developers E.');
+    cy.get('blockquote').contains('http://www.eeacitation.com');
+    cy.get('.citation-block a').first().next().click();
+    cy.get('pre').contains(
+      'Developers E. (2023). Citation. http://www.eeacitation.com',
+    );
+    cy.get('.citation-block a').first().next().next().click();
+    cy.get('pre').contains('TY  - JOUR');
+    cy.get('pre').contains('DA  - 2023///');
+    cy.get('pre').contains('TI  - Citation');
+    cy.get('pre').contains('UR  - http://www.eeacitation.com');
+    cy.get('.citation-block a').first().next().next().next().click();
+    cy.get('pre').contains('author = {{Developers E.}},');
+    cy.get('pre').contains('year = {2023},');
+    cy.get('pre').contains('title = {Citation},');
+    cy.get('pre').contains('howpublished = {http://www.eeacitation.com},');
+
+    //test url
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
   });
 });
